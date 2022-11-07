0bee2b3491375211b9e8868bcb8583d5a2469ea9
GBSeo <117253554+GBSeo@users.noreply.github.com>
2022-11-07 22:36:48 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,7 @@ var videoStorage = [
     './video/1.mp4',
     './video/2.mp4',
     './video/3.mp4',
+    './video/4.mp4',
 ]
 video = document.querySelector('video');
     // choose one random url from our storage as the active video
